a3d128678778a5b361e877a6cc733741ee718dbc
yamatohagi <53859640+yamatohagi@users.noreply.github.com>
2021-07-06 00:22:20 +0900

    Update test.user.js

--- test.user.js
+++ test.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         test
 // @namespace    http://tampermonkey.net/
-// @version      0.16
+// @version      0.16001
 // @description  try to take over the world!
 // @author       yamatohagi
 // @match        https://*/*

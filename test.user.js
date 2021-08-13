d8058494faefc325a70a658f09b090b4c662c718
pncs33 <53859640+yamatohagi@users.noreply.github.com>
2021-08-13 20:58:59 +0900

    ポップアップ

--- test.user.js
+++ test.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         test
 // @namespace    http://tampermonkey.net/
-// @version      0.16037
+// @version      0.16038
 // @description  try to take over the world!
 // @author       yamatohagi
 // @match        https://*/*
@@ -15,7 +15,6 @@ console.log(mwurl);
 //////////////////////////////////////////【MMMMWWWW】////////////////////////////【MMMMWWWW】/////////////////
 if (mwurl.match(/^(?=.*MailView)(?=.*japan)/)) {
     let $ = window.jQuery;
-    //変数宣言
     let clientMailAd = '';
     let befoe = '';
     let maillorFon = '';
@@ -28,106 +27,89 @@ if (mwurl.match(/^(?=.*MailView)(?=.*japan)/)) {
         clientMailAd = document.querySelector("#mail-body-area > div > div > div:nth-child(3)").textContent;
         if (clientMailAd.match(/メールアドレス】/)) {
             clientMailAd = clientMailAd.match(/(?<=メールアドレス】)(.*)/)[0];
+            maillorFon = 0
         }
         if (clientMailAd.match(/電話番号】/)) {
-            clientMailAd = clientMailAd.match(/(?<=電話番号】)(.*)/)[0];//とりあえずなし
+            clientMailAd = clientMailAd.match(/(?<=電話番号】)(.*)/)[0];
+            maillorFon = 1
         }
     }
-    //関数
-        function clientSearchButton() {
-            navigator.clipboard.writeText(clientMailAd);
+    function clientSearchButton() {
+        navigator.clipboard.writeText(clientMailAd);
+        if (maillorFon == 0) {
             setTimeout(function () {
-                        const replaced = clientMailAd.replace('@', '%40')
-            befoe = (atob("aHR0cHM6Ly93d3cucGFydHlwYXJ0eS5qcC9hZG1pbi9tZW1iZXJzL2xpc3Q/ZW50cnlfdHlwZT0mcGFydHlfaWQ9Jm1lbWJlcl9zZWFyY2glNUJ0cmFuc2Zlcl9wYXJ0eV9pZCU1RD0mbWVtYmVyX3NlYXJjaCU1QmZyZWV3b3JkJTVEPQ=="));
-            var adURL = `${befoe}${replaced}&member_search%5Bfreeword_target%5D=2&member_search%5Bblacklist_search_code%5D=&member_search%5Badmin_user_id%5D=&member_search%5Bmember_type_cd%5D=&member_search%5Bdelete_flg%5D=&member_search%5Bgender_cd%5D=&member_search%5Bage_from%5D=&member_search%5Bage_to%5D=&member_search%5Bbirthday_year%5D=&member_search%5Bbirthday_month%5D=&member_search%5Bbirthday_day%5D=&member_search%5Bmarital_history_cd%5D=&member_search%5Blast_login_at_from%5D=&member_search%5Blast_login_at_to%5D=&member_search%5Bparty_total_entry_from%5D=&member_search%5Bparty_total_entry_to%5D=&member_search%5Boption%5D=9&member_search%5Bblock_cd%5D=&member_search%5Bsales_section%5D=&member_search%5Bfreeword2%5D=&ykmshg`
-            window.open(adURL, '_blank');
+                const replaced = clientMailAd.replace('@', '%40')
+                befoe = (atob("aHR0cHM6Ly93d3cucGFydHlwYXJ0eS5qcC9hZG1pbi9tZW1iZXJzL2xpc3Q/ZW50cnlfdHlwZT0mcGFydHlfaWQ9Jm1lbWJlcl9zZWFyY2glNUJ0cmFuc2Zlcl9wYXJ0eV9pZCU1RD0mbWVtYmVyX3NlYXJjaCU1QmZyZWV3b3JkJTVEPQ=="));
+                var adURL = `${befoe}${replaced}&member_search%5Bfreeword_target%5D=2&member_search%5Bblacklist_search_code%5D=&member_search%5Badmin_user_id%5D=&member_search%5Bmember_type_cd%5D=&member_search%5Bdelete_flg%5D=&member_search%5Bgender_cd%5D=&member_search%5Bage_from%5D=&member_search%5Bage_to%5D=&member_search%5Bbirthday_year%5D=&member_search%5Bbirthday_month%5D=&member_search%5Bbirthday_day%5D=&member_search%5Bmarital_history_cd%5D=&member_search%5Blast_login_at_from%5D=&member_search%5Blast_login_at_to%5D=&member_search%5Bparty_total_entry_from%5D=&member_search%5Bparty_total_entry_to%5D=&member_search%5Boption%5D=9&member_search%5Bblock_cd%5D=&member_search%5Bsales_section%5D=&member_search%5Bfreeword2%5D=&ykmshg`
+                window.open(adURL, '_blank');
             }, 10);
-      }
-        $('.dataView').append($('<input type="button" id="id01" value="会員検索" style="width:90px;height:30px;font-size:18px;background:#FF66CC;" >').click(function () {
-            navigator.clipboard.writeText("ER");
-            clientSearchButton();
-        }));;
+        } else {
+            setTimeout(function () {
+                befoe = (atob("aHR0cHM6Ly93d3cucGFydHlwYXJ0eS5qcC9hZG1pbi9tZW1iZXJzL2xpc3Q/ZW50cnlfdHlwZT0mcGFydHlfaWQ9Jm1lbWJlcl9zZWFyY2glNUJ0cmFuc2Zlcl9wYXJ0eV9pZCU1RD0mbWVtYmVyX3NlYXJjaCU1QmZyZWV3b3JkJTVEPQ=="));
+                var adURL = `${befoe}${clientMailAd}&member_search%5Bfreeword_target%5D=5&member_search%5Bblacklist_search_code%5D=&member_search%5Badmin_user_id%5D=&member_search%5Bmember_type_cd%5D=&member_search%5Bdelete_flg%5D=&member_search%5Bgender_cd%5D=&member_search%5Bage_from%5D=&member_search%5Bage_to%5D=&member_search%5Bbirthday_year%5D=&member_search%5Bbirthday_month%5D=&member_search%5Bbirthday_day%5D=&member_search%5Bmarital_history_cd%5D=&member_search%5Blast_login_at_from%5D=&member_search%5Blast_login_at_to%5D=&member_search%5Bparty_total_entry_from%5D=&member_search%5Bparty_total_entry_to%5D=&member_search%5Boption%5D=9&member_search%5Bblock_cd%5D=&member_search%5Bsales_section%5D=&member_search%5Bfreeword2%5D=&ykmshg`
+                window.open(adURL, '_blank');
+            }, 10);
+        }
+    }
+    $('.dataView').append($('<input type="button" id="id01" value="会員検索" style="width:90px;height:30px;font-size:18px;background:#FF66CC;" >').click(function () {
+        navigator.clipboard.writeText("ER");
+        clientSearchButton();
+    }));;
 }
-///////////////////////【mw to ykmshg】//////////////////////////////////////【mw to ykmshg】//////////////////////////////////////【mw to ykmshg】///////////////
+///////////////////////【mw to ykmshg】///////////////////////////////////
 if (mwurl.match(/^(?=.*ty.jp)(?=.*members)(?=.*admin)(?=.*ykmshg)/)) {
-    if(document.getElementsByClassName('container mb-30')[0].textContent.match(/1件を表示/)){
+    if (document.getElementsByClassName('container mb-30')[0].textContent.match(/1件を表示/)) {
         document.querySelector("body > div.container.mb-30 > table > tbody > tr > td:nth-child(1) > a").click()
     }
 }
-///////////////////////【mw to ykmshg】//////////////////////////////////////【mw to ykmshg】//////////////////////////////////////【mw to ykmshg】///////////////
-
-
+///////////////////////【mw to ykmshg】/////////////////////////////////
 //////////////////////////////////////////【admin】/////////////////////////////////////////////////【admin】/////////////////////////////////////////////////【admin】/////////////////////////////////////////////////【admin】//////////////////////////////
 if (mwurl.match(/^(?=.*ty.jp)(?=.*members)(?=.*admin)/)) {
     //定形宣言
     let $ = window.jQuery;
     let element = '';
     //////////______________________________________会員詳細情報画面のみ
-    let result = '';
     let maillcopyclientfield = '';
-    let namecopyclientfield = '';
-    let clientsyousai = document.querySelector("body > div > h2")
-    var clientsyousaiText = clientsyousai.textContent
-    if (clientsyousaiText.match(/会員詳細情報/)) {
-        //ここに処理
+    if (document.querySelector("body > div > h2").textContent.match(/会員詳細情報/)) {
         console.log("会員詳細情報が実行されています");
         $('th.background-lightgray:eq(6)').append($('<input type="button" id="id001" value="メアドコピー" style="width:80px;height:25px;font-size:8px;background:#3399FF;" >').click(function () {
-            document.getElementById("id001").value = ("copy済")
             document.getElementById("id001").style.background = "#CCCCCC"
             setTimeout(function () {
-                document.getElementById("id001").value = ("メアドコピー")
                 document.getElementById("id001").style.background = "#3399FF"
             }, 2000);
-            maillcopyclientfield = document.getElementsByClassName('table table-sm table-bordered')[0]
-            result = maillcopyclientfield.textContent.match(/メールアドレス(.*)連絡用メールアドレス/);
-            if (result != null) {
-                maillcopyclientfield = (result[1]);
-                //合致内容があればresultに格納
-                if (navigator.clipboard) {
-                    navigator.clipboard.writeText(maillcopyclientfield);
-                    //格納をコピー
-                    console.log(maillcopyclientfield);
-                }
-            }
+            maillcopyclientfield = document.getElementsByClassName('table table-sm table-bordered')[0].textContent.match(/メールアドレス(.*)連絡用メールアドレス/)[1];
+            if (maillcopyclientfield != (null||'')) {
+                navigator.clipboard.writeText(maillcopyclientfield);
+                console.log(maillcopyclientfield);
+            }else{
+                navigator.clipboard.writeText('ER');
+            }            
         }));;
         $('th.background-lightgray:eq(2)').append($('<input type="button" id="id0003" value="フルネーム" style="width:70px;height:27px;font-size:8px;background:#FFCC66;" >').click(function () {
-            document.getElementById("id0003").value = ("copy済")
             document.getElementById("id0003").style.background = "#CCCCCC"
             setTimeout(function () {
-                document.getElementById("id0003").value = ("フルネーム")
                 document.getElementById("id0003").style.background = "#FFCC66"
             }, 2000);
-            let fullNameString = document.getElementsByClassName('table table-sm table-bordered')[0].textContent
-            result = fullNameString.match(/姓名（ふりがな）(.*)\s[（(]/);
-            if (result != null) {
-                let clientname1 = (result[1]);
-                //合致内容があればresultに格納
-                if (navigator.clipboard) {
-                    navigator.clipboard.writeText(clientname1);
-                    //格納をコピー
-                    console.log(clientname1);
-                }
-            }
+            let fullNameString = document.getElementsByClassName('table table-sm table-bordered')[0].textContent.match(/姓名（ふりがな）(.*)\s[（(]/);
+            if (fullNameString[1] != null) {
+                    navigator.clipboard.writeText(fullNameString[1]);
+                    console.log(fullNameString[1]);
+            }else{
+                navigator.clipboard.writeText('ER');
+            }    
         }));;
         $('th.background-lightgray:eq(2)').append($('<input type="button" id="id0001" value="苗字" style="width:50px;height:27px;font-size:8px;background:#00CC33;" >').click(function () {
-            document.getElementById("id0001").value = "copy済"
             document.getElementById("id0001").style.background = "#CCCCCC"
             setTimeout(function () {
-                document.getElementById("id0001").value = ("苗字")
                 document.getElementById("id0001").style.background = "#00CC33"
             }, 2000);
-            var tagetString = document.getElementsByClassName('table table-sm table-bordered')[0].textContent
-            result = tagetString.match(/姓名（ふりがな）(.*)\s[（(]/);
-            result = result[1].split(/\s+/);
-            console.log(result[0]);
-            if (result != null) {
-                let clientname1 = (result[0]);
-                //合致内容があればresultに格納
-                if (navigator.clipboard) {
-                    navigator.clipboard.writeText(clientname1);
-                    //格納をコピー
-                }
-            }
+            var tagetString = document.getElementsByClassName('table table-sm table-bordered')[0].textContent.match(/姓名（ふりがな）(.*)\s[（(]/)[1].split(/\s+/)[0];
+            if (tagetString != null) {
+                console.log(tagetString);
+                    navigator.clipboard.writeText(tagetString);
+            }else{
+                navigator.clipboard.writeText('ER');
+            }    
         }));;
     }
     //////////______________________________________会員詳細情報画面のみ/////______________________________________会員詳細情報画面のみ/////______________________________________会員詳細情報画面のみ
@@ -172,7 +154,7 @@ if (mwurl.match(/^(?=.*ty.jp)(?=.*members)(?=.*admin)/)) {
 
 ///////【member_search】/////////////【member_search】///////
 if (mwurl.match(/^(?=.*member_search)(?=.*members)(?=.*admin)/)) {
-    if (document.getElementsByClassName('text-center')[2].textContent == "該当の検索結果はありません"){
+    if (document.getElementsByClassName('text-center')[2].textContent == "該当の検索結果はありません") {
         setTimeout(function () {
             location.href = (atob("aHR0cHM6Ly93d3cucGFydHlwYXJ0eS5qcC9hZG1pbi9tZW1iZXJz"));
         }, 500);

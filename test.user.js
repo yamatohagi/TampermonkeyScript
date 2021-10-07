c2b85733badaf0ada8c411b900b54c63e1a3b827
pncs33 <53859640+yamatohagi@users.noreply.github.com>
2021-10-07 18:25:03 +0900

    エクスポート外し

--- test.user.js
+++ test.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         test
 // @namespace    http://tampermonkey.net/
-// @version      0.16060
+// @version      0.16061
 // @description  try to take over the world!
 // @author       yamatohagi
 // @match        https://*/*
@@ -479,80 +479,6 @@ if (mwurl.match(/^(?=.*admin)(?=.*smartdevice_parties)/)) {
     var ProgressdaytimeF = document.getElementsByClassName('container mb-30')[0].textContent.match(/\d{2}(?=:)/)
     var ProgressdaytimeB = document.getElementsByClassName('container mb-30')[0].textContent.match(/(?<=:)\d{2}/)
     document.title = `【進行】[${ProgressdaytimeF} : ${ProgressdaytimeB}]`;//タブ名
-    if (mwurl.match(/^(?=.*detail)(?=.*smartdevice_parties)/)) {//結果エクスポート
-        $('.text-left:eq(0)').append($('<input type="button" id="id1153" value="エクスポート" style="width:90px;height:27px;font-size:13px;background:#F08080;" >').click(function () {
-            function manExport() {
-                var woman = document.querySelector("body > div > div:nth-child(6) > div > table > tbody").getElementsByTagName('tr').length - 1
-                //女性の人数把握
-                var manjoin = document.querySelector("body > div > div:nth-child(6) > div > table > tbody > tr:nth-child(1)").getElementsByTagName('th').length - 1
-                //男性の人数把握
-                var dict = [];
-                for (var man = 2; man < manjoin + 2; man++) {
-                    var ary = [];
-                    for (var manNumvertical = 2; manNumvertical < woman + 2; manNumvertical++) {
-                        var goodnum = document.querySelector(`body > div > div:nth-child(6) > div > table > tbody > tr:nth-child(${manNumvertical}) > td:nth-child(${man})`).getElementsByClassName('col-sm-6')[1].textContent
-                        goodnum = goodnum || '0'
-                        ary.push(goodnum)
-                    }
-                    ////
-
-                    var intMax = 0; //配列の最大値が0の場合もあるため、配列の要素の1番目で初期化すること
-
-                    for (var i = 0; i < ary.length; i++) {　// 要素0番目のは代入済みのため1番目から開始する
-
-                        //intMaxに代入されている値と配列の要素を比較して、配列の要素のほうが大きい場合値を上書きする.
-                        if (intMax < ary[i]) {
-                            intMax = ary[i]
-                        }
-                    }
-                    var numranking = []
-                    for (var inum = 1; inum <= intMax; inum++) {
-                        var kari = ary.indexOf(`${inum}`)
-                        numranking.push(kari + 1)
-                    }
-                    ///
-                     dict.push(numranking);
-                }
-                 console.log(dict)
-                console.log(dict.join('\n'))
-            }
-            function womanExport() {
-                var woman = document.querySelector("body > div > div:nth-child(6) > div > table > tbody").getElementsByTagName('tr').length - 1
-                //女性の人数把握
-                var man = document.querySelector("body > div > div:nth-child(6) > div > table > tbody > tr:nth-child(1)").getElementsByTagName('th').length - 1
-                //男性の人数把握
-                var dict = [];
-                for (var womanNum = 2; womanNum < woman + 2; womanNum++) {
-                    var lineElement = document.querySelector(`body > div > div:nth-child(6) > div > table > tbody > tr:nth-child(${womanNum})`)
-                    var ary = [];
-                    for (var womanNumvertical = 2; womanNumvertical < man + 2; womanNumvertical++) {
-                        var goodnum = lineElement.querySelector(`td:nth-child(${womanNumvertical})`).getElementsByClassName('col-sm-6')[0].textContent
-                        goodnum = goodnum || '0'
-                        ary.push(goodnum)
-                    }
-                    var intMax = 0; //配列の最大値が0の場合もあるため、配列の要素の1番目で初期化すること
-
-                    for (var i = 0; i < ary.length; i++) {　// 要素0番目のは代入済みのため1番目から開始する
-                        //intMaxに代入されている値と配列の要素を比較して、配列の要素のほうが大きい場合値を上書きする.
-                        if (intMax < ary[i]) {
-                            intMax = ary[i]
-                        }
-                    }
-                    var numranking = []
-                    for (var iwoman = 1; iwoman <= intMax; iwoman++) {
-                        var kari = ary.indexOf(`${iwoman}`)
-                        numranking.push(kari + 1)
-                    }
-                    ///
-                    dict.push(numranking);
-                }
-                console.log(dict)
-                console.log(dict.join('\n'))
-            }
-            manExport()
-            womanExport()            
-        }))
-    }
 }
 if (mwurl.match(/^(?=.*admin)(?=.*smart_parties)/)) {
     var ProgressdaytimeSmartF = document.getElementsByClassName('container mb-30')[0].textContent.match(/\d{2}(?=:)/)

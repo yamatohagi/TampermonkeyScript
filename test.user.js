63b669e1c350ce3d22093ae47fe1228e86ece27c
yama <53859640+yamatohagi@users.noreply.github.com>
2022-07-02 21:48:27 +0900

    バージョン

--- test.user.js
+++ test.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         test
 // @namespace    http://tampermonkey.net/
-// @version      0.17
+// @version      0.16069
 // @description  try to take over the world!
 // @author       yamatohagi
 // @match        https://*/*
